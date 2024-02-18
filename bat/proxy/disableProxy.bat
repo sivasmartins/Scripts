f40e7fb2421e8a5ab7019ac364b4e95e49320e01
Sivaldo Martins <sivasmartins@gmail.com>
2024-02-18 09:40:37 -0300

    Update disableProxy.bat

--- bat/proxy/disableProxy.bat
+++ bat/proxy/disableProxy.bat
@@ -1,14 +1,9 @@
 @echo off
 ::
-:: disableProxy.bat
-:: 
-:: Habilitar o proxy no Windows
+:: Name: disableProxy.bat
+:: Language: bat
+:: Function: Disable internet proxy on Windows
 ::
-:: Modo de usar: 
-::   
-::  disableProxy.bat
-:: 
-:: Autor: Sivaldo Martins
-::
+
 REG ADD "HKCU\Software\Microsoft\Windows\CurrentVersion\Internet Settings" /v proxyserver /t REG_SZ /d "" /f
 REG ADD "HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Internet Settings" /v ProxyEnable /t REG_DWORD /d 0x00000000 /f
